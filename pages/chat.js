b33ecab210d82e2a10d1f6dd6ab279a96bbb947d
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2022-01-25 13:08:44 -0300

    [UPDATE] Aula 2
    - Usar state do React para manipular variaveis reativas;
    - Cria rota de chat
    - Desafio: usuario deve digitar mais de 2 caracteres para mostrar imagem de perfil e nome de usuario

---
+++ pages/chat.js
@@ -0,0 +1,36 @@
+import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import appConfig from '../config.json'
+
+export default function ChatPage(){
+    return(
+        <>
+       
+        <Box
+        styleSheet={{
+            display: 'flex', alignItems: 'center', justifyContent: 'center',
+            backgroundColor: appConfig.theme.colors.primary[500],
+            backgroundImage: 'url(https://images6.alphacoders.com/118/thumb-1920-1185407.jpg)',
+            backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+        }}
+    >
+        <Box
+            styleSheet={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                flexDirection: {
+                    xs: 'column',
+                    sm: 'row',
+                },
+                width: '100%', maxWidth: '700px',
+                borderRadius: '5px', padding: '32px', margin: '16px',
+                boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+                backgroundColor: appConfig.theme.colors.neutrals[700],
+            }}
+        >
+            <h1>Chat messages</h1>
+            </Box>
+            </Box>
+            </>
+    )
+}

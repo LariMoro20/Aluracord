c3c9740d27d8dd6ce98f1f797045a63d0155aada
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2022-01-29 18:16:14 -0300

    Update Messages.js

--- components/Messages/Messages.js
+++ components/Messages/Messages.js
@@ -6,7 +6,15 @@ import React, { useEffect, useRef }  from 'react';
 function MessageItem(props) {
     //const [message, setMessage] = React.useState();
 
-    
+    const messagesEndRef = useRef(null);
+
+    const scrollToBottom = () => {
+      messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    };
+
+    useEffect(() => {
+        scrollToBottom();
+      }, [props]);
    
    // const [src, setSrc] = React.useState(props.src);
     return (
@@ -60,22 +68,14 @@ function MessageItem(props) {
                 )}
 
             </Text>
-           
+            <div ref={messagesEndRef} />
         </Box>
     )
 }
 
 export default function Messages(props) {
    
-    const messagesEndRef = useRef(null);
 
-    const scrollToBottom = () => {
-      messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
-    };
-
-    useEffect(() => {
-        scrollToBottom();
-      }, [props.itens]);
     return (
         <Box tag='div'
             styleSheet={{
@@ -90,7 +90,6 @@ export default function Messages(props) {
                     <MessageItem text={msg.text} from={msg.from} key={msg.id} id={msg.id} data={msg.created_at} userlogged={props.userlogged} removeMessage={props.removeMessage} />
                 )
             })}
-             <div ref={messagesEndRef} />
         </Box>
     )
 }
